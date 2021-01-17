e41eb151da59cf9374f86c317575fe8367b28f36
SStupar2 <stefan.stupar2@gmail.com>
2021-01-17 10:40:01 +0100

    initial commit

---
+++ monolitna/src/main/java/com/example/monolitna/MonolitnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.monolitna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitnaApplication.class, args);
+	}
+
+}
---
+++ monolitna/src/test/java/com/example/monolitna/MonolitnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monolitna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolitnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
